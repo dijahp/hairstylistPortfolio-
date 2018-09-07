e070b75c286b25955a89329e379f73df1827a81a
Khadijah Parks <dijah@bellsouth.net>
2018-09-07 13:17:26 -0400

    Update porfolio.js

--- JS/porfolio.js
+++ JS/porfolio.js
@@ -1,7 +1,7 @@
-// Get the modal
+
 var modal = document.getElementById('lidaModal');
 
-// Get the image and insert it inside the modal - use its "alt" text as a caption
+
 var img = $('.myImg');
 var modalImg = $("#img1");
 
@@ -13,10 +13,10 @@ $('.myImg').click(function(){
   captionText.innerHTML = this.alt;
 });
 
-// Get the <span> element that closes the modal
+
 var span = document.getElementsByClassName("close")[0];
 
-// When the user clicks on <span> (x), close the modal
+
 span.onclick = function() { 
   modal.style.display = "none";
 }
